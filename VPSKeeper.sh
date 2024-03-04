e1134978f9a8d90f950d0944c4ff8fc50d931fed
redstarxxx <150769395+redstarxxx@users.noreply.github.com>
2024-03-04 19:26:17 +0800

    Update VPSKeeper.sh

--- VPSKeeper.sh
+++ VPSKeeper.sh
@@ -592,7 +592,7 @@ while true; do
         top_output=\$(top -n 1 -b | awk 'NR > 7')
         cpu_h1=\$(echo "\$top_output" | awk 'NR == 1 || \$9 > max { max = \$9; process = \$12 } END { print process }')
         cpu_h2=\$(echo "\$top_output" | awk 'NR == 2 || \$9 > max { max = \$9; process = \$12 } END { print process }')
-        message="CPU 使用率超过阀值 > $CPUThreshold%❗️"\$'\n'"主机名: \$(hostname)"\$'\n'"CPU 当前使用率: \$cpu_usage%"\$'\n'"使用率排行: 1.\$cpu_h1 2.\$cpu_h2"\$'\n'"检测工具: $CPUTools"\$'\n'"休眠时间: \$((SleepTime / 60))分钟"
+        message="CPU 使用率超过阀值 > $CPUThreshold%❗️"\$'\n'"主机名: \$(hostname)"\$'\n'"CPU 当前使用率: \$cpu_usage%"\$'\n'"使用率排行: 1:\$cpu_h1 2:\$cpu_h2"\$'\n'"检测工具: $CPUTools"\$'\n'"休眠时间: \$((SleepTime / 60))分钟"
         curl -s -X POST "https://api.telegram.org/bot$TelgramBotToken/sendMessage" -d chat_id="$ChatID_1" -d text="\$message" > /dev/null
         echo "报警信息已发出..."
         count=0  # 发送警告后重置计数器
@@ -633,7 +633,7 @@ while true; do
         top_output=\$(top -n 1 -b | awk 'NR > 7')
         cpu_h1=\$(echo "\$top_output" | awk 'NR == 1 || \$9 > max { max = \$9; process = \$12 } END { print process }')
         cpu_h2=\$(echo "\$top_output" | awk 'NR == 2 || \$9 > max { max = \$9; process = \$12 } END { print process }')
-        message="CPU 使用率超过阀值 > $CPUThreshold%❗️"\$'\n'"主机名: \$(hostname)"\$'\n'"CPU 当前使用率: \$cpu_usage%"\$'\n'"使用率排行: 1.\$cpu_h1 2.\$cpu_h2"\$'\n'"检测工具: $CPUTools"\$'\n'"休眠时间: \$((SleepTime / 60))分钟"
+        message="CPU 使用率超过阀值 > $CPUThreshold%❗️"\$'\n'"主机名: \$(hostname)"\$'\n'"CPU 当前使用率: \$cpu_usage%"\$'\n'"使用率排行: 1:\$cpu_h1 2:\$cpu_h2"\$'\n'"检测工具: $CPUTools"\$'\n'"休眠时间: \$((SleepTime / 60))分钟"
         curl -s -X POST "https://api.telegram.org/bot$TelgramBotToken/sendMessage" -d chat_id="$ChatID_1" -d text="\$message" > /dev/null
         echo "报警信息已发出..."
         count=0  # 发送警告后重置计数器
@@ -677,7 +677,7 @@ while true; do
         top_output=\$(top -n 1 -b | awk 'NR > 7')
         cpu_h1=\$(echo "\$top_output" | awk 'NR == 1 || \$9 > max { max = \$9; process = \$12 } END { print process }')
         cpu_h2=\$(echo "\$top_output" | awk 'NR == 2 || \$9 > max { max = \$9; process = \$12 } END { print process }')
-        message="CPU 使用率超过阀值 > $CPUThreshold%❗️"\$'\n'"主机名: \$(hostname)"\$'\n'"CPU 当前使用率: \$cpu_usage%"\$'\n'"使用率排行: 1.\$cpu_h1 2.\$cpu_h2"\$'\n'"检测工具: $CPUTools"\$'\n'"休眠时间: \$((SleepTime / 60))分钟"
+        message="CPU 使用率超过阀值 > $CPUThreshold%❗️"\$'\n'"主机名: \$(hostname)"\$'\n'"CPU 当前使用率: \$cpu_usage%"\$'\n'"使用率排行: 1:\$cpu_h1 2:\$cpu_h2"\$'\n'"检测工具: $CPUTools"\$'\n'"休眠时间: \$((SleepTime / 60))分钟"
         curl -s -X POST "https://api.telegram.org/bot$TelgramBotToken/sendMessage" -d chat_id="$ChatID_1" -d text="\$message" > /dev/null
         echo "报警信息已发出..."
         count=0  # 发送警告后重置计数器
