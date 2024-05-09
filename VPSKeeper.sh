d3a9ad8be156935cc414f8117048edbc69060d5e
redstarxxx <150769395+redstarxxx@users.noreply.github.com>
2024-05-09 22:12:02 +0800

    Update VPSKeeper.sh

--- VPSKeeper.sh
+++ VPSKeeper.sh
@@ -28,7 +28,7 @@ else
 fi
 
 # 基本参数
-sh_ver="1.240509.1"
+sh_ver="1.240509.2"
 FolderPath="/root/.shfile"
 ConfigFile="/root/.shfile/TelgramBot.ini"
 BOTToken_de="6718888288:AAG5aVWV4FCmS0ItoPy1-3KkhdNg8eym5AM"
